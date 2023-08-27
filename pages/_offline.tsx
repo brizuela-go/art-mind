37e4ad82068052a06191ffd41557aa66ee3d6c2b
Juan Brizuela <juanbrizuelago@gmail.com>
2023-08-27 15:02:59 -0600

    offline fallback

---
+++ pages/_offline.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const offline = (props: Props) => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center justify-center items-center h-screen">
+        Estás en modo offline 😔
+      </h1>
+    </div>
+  );
+};
+
+export default offline;
